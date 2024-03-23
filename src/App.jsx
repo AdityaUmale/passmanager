eba188c9603109145d7c8633facd07fd88de46b1
Aditya Umale <aditya.umale17@gmail.com>
2024-03-23 12:49:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <div>
+        my ui
+      </div>
+    </>
+  )
+}
+
+export default App
